ce9f76cdd246cd536f58e6db96743f8bafc1471f
leepand <leepand@leepanddeMacBook-Pro.local>
2023-04-24 14:49:43 +0800

    update dynaQ+

--- minirl/__init__.py
+++ minirl/__init__.py
@@ -4,4 +4,4 @@ from .preprocessing.scaler import StandardScaler as StdScaler
 from .preprocessing.scaler import MinMaxScaler
 from .preprocessing.scaler import MaxAbsScaler
 from .core.bandit import BanditEEAgent
-from .core.dynaQ_plus import DynaQPlus
+from .core.dynaQ_plus import DynaQPlus,simpleModel,TimeModel
